e83556f6f97c76c372ddea144ff360ac57bf91b7
Brent Yoder <499565+InAbsentia@users.noreply.github.com>
2024-01-26 10:17:52 -0700

    Upgrade elixir-ls

--- .config/nvim/lua/plugins/elixir.lua
+++ .config/nvim/lua/plugins/elixir.lua
@@ -22,7 +22,7 @@ return {
         credo = { enable = true },
         elixirls = {
           enable = true,
-          tag = "v0.17.1",
+          tag = "v0.19.0",
           settings = elixirls.settings({
             dialyzerEnabled = false,
             enableTestLenses = true,
