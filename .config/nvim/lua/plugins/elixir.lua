6d20d64017b7a8ef109a82e8cf5fc4240c7ef48e
Brent Yoder <499565+InAbsentia@users.noreply.github.com>
2024-06-25 16:37:31 -0600

    Disable elixir-ls in nvim

--- .config/nvim/lua/plugins/elixir.lua
+++ .config/nvim/lua/plugins/elixir.lua
@@ -10,30 +10,45 @@ return {
     end,
   },
   {
+    -- "elixir-tools/elixir-tools.nvim",
     "inabsentia/elixir-tools.nvim",
-    -- dir = "~/Developer/elixir-tools.nvim",
     version = "*",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local elixir = require("elixir")
-      local elixirls = require("elixir.elixirls")
+      -- local elixirls = require("elixir.elixirls")
 
       elixir.setup({
-        nextls = { enable = true },
+        nextls = {
+          enable = true,
+          init_options = {
+            -- mix_env = "dev",
+            -- mix_target = "host",
+            experimental = {
+              completions = {
+                enable = true,
+              },
+            },
+          },
+          -- on_attach = function(client, bufnr)
+          --   -- custom keybinds
+          -- end,
+        },
         credo = { enable = true },
         elixirls = {
-          enable = true,
-          tag = "v0.21.0",
-          settings = elixirls.settings({
-            dialyzerEnabled = true,
-            incrementalDialyzer = true,
-            enableTestLenses = true,
-          }),
-          on_attach = function(client, bufnr)
-            vim.keymap.set("n", "<space>cnp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("n", "<space>cnP", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-            vim.keymap.set("v", "<space>cnM", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-          end,
+          enable = false,
+          -- tag = "v0.22.0",
+          -- settings = elixirls.settings({
+          --   dialyzerEnabled = true,
+          --   incrementalDialyzer = true,
+          --   enableTestLenses = false,
+          --   suggestSpecs = false,
+          -- }),
+          -- on_attach = function(client, bufnr)
+          --   vim.keymap.set("n", "<space>cnp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+          --   vim.keymap.set("n", "<space>cnP", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+          --   vim.keymap.set("v", "<space>cnM", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          -- end,
         },
       })
     end,
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,10 +5,11 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "elm-format",
         "elm-language-server",
-        "erlang-ls",
+        -- "erlang-ls",
         "graphql-language-service-cli",
         "html-lsp",
         "js-debug-adapter",
+        -- "lexical",
         "lua-language-server",
         "markdown-toc",
         "markdownlint",
