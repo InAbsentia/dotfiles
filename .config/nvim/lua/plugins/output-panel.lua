73e505106dc113ccdca983780b9cfcdff6ba062c
Brent Yoder <499565+InAbsentia@users.noreply.github.com>
2025-01-20 00:46:14 -0700

    Fix output-panel config

--- .config/nvim/lua/plugins/output-panel.lua
+++ .config/nvim/lua/plugins/output-panel.lua
@@ -1,10 +1,11 @@
 return {
   "mhanberg/output-panel.nvim",
+  version = "*",
   event = "VeryLazy",
   keys = {
     { "<leader>clo", ":OutputPanel<cr>", { desc = "Open LSP Output" } },
   },
   config = function()
-    require("output_panel").setup()
+    require("output_panel").setup({})
   end,
 }
