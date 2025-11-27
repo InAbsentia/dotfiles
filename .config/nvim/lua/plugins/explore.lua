d3da179e5f1d3682921c3d1f1f99b64d60c945de
Brent Yoder <499565+InAbsentia@users.noreply.github.com>
2025-11-26 19:57:52 -0700

    Add Oil commands to show hidden or details

--- .config/nvim/lua/plugins/explore.lua
+++ .config/nvim/lua/plugins/explore.lua
@@ -1,10 +1,43 @@
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
+local show_detail = false
+
 return {
   {
     "stevearc/oil.nvim",
     lazy = false,
     opts = {
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
       keymaps = {
         ["q"] = "actions.close",
+        ["gh"] = {
+          desc = "Toggle hidden files",
+          callback = function()
+            require("oil").toggle_hidden()
+          end,
+        },
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            show_detail = not show_detail
+            if show_detail then
+              require("oil").set_columns({ "permissions", "size", "mtime", "icon" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
     },
     keys = {
